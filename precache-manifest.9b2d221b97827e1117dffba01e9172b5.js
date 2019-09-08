6b36e0f354da1d57e0461c79395dc5e3a4c3c86e
viccyviccy <iamviccyviccy@gmail.com>
2019-09-08 15:15:17 +0300

    Updates

---
+++ precache-manifest.9b2d221b97827e1117dffba01e9172b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd2bf3d9195db68dc39202ede5035e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3008917ff260fe527770",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "a8795b79462023a20049",
+    "url": "/static/css/main.9e21676a.chunk.css"
+  },
+  {
+    "revision": "3008917ff260fe527770",
+    "url": "/static/js/2.7d2cb46c.chunk.js"
+  },
+  {
+    "revision": "a8795b79462023a20049",
+    "url": "/static/js/main.87b68eae.chunk.js"
+  },
+  {
+    "revision": "2852590948277b1ff15f",
+    "url": "/static/js/runtime~main.b87843b6.js"
+  }
+]);
